b83d0fde528941ba302c560b582465904afc3bed
taylor d <taylor.delph@eyecuelab.com>
2022-04-24 15:09:49 -0700

    add create new joke route

---
+++ app/routes/jokes/new.tsx
@@ -0,0 +1,24 @@
+export default function NewJokeRoute() {
+  return (
+    <div>
+      <p>Add your own hilarious joke</p>
+      <form method="post">
+        <div>
+          <label>
+            Name: <input type="text" name="name" placeholder="Name"/>
+          </label>
+        </div>
+        <div>
+          <label>
+            Content: <textarea name="content" placeholder="Content"/>
+          </label>
+        </div>
+        <div>
+          <button type="submit" className="button">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
