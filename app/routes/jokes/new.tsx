adfc7bf939983cc2a3c43b5c75fc1b3975a48570
taylor d <taylor.delph@eyecuelab.com>
2022-04-27 09:00:15 -0700

    add basic post method to form

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,3 +1,29 @@
+import type { ActionFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+
+import { db } from "~/utils/db.server";
+
+export const action: ActionFunction = async ({
+  request,
+}) => {
+  const form = await request.formData();
+  const name = form.get("name");
+  const content = form.get("content");
+  // we do this type check to be extra sure and to make TypeScript happy
+  // we'll explore validation next!
+  if (
+    typeof name !== "string" ||
+    typeof content !== "string"
+  ) {
+    throw new Error(`Form not submitted correctly.`);
+  }
+
+  const fields = { name, content };
+
+  const joke = await db.joke.create({ data: fields });
+  return redirect(`/jokes/${joke.id}`);
+};
+
 export default function NewJokeRoute() {
   return (
     <div>
@@ -5,12 +31,12 @@ export default function NewJokeRoute() {
       <form method="post">
         <div>
           <label>
-            Name: <input type="text" name="name" placeholder="Name"/>
+            Name: <input type="text" name="name" />
           </label>
         </div>
         <div>
           <label>
-            Content: <textarea name="content" placeholder="Content"/>
+            Content: <textarea name="content" />
           </label>
         </div>
         <div>
