d182e53f07c3a2292473c8e74b00341723f802d9
taylor d <taylor.delph@eyecuelab.com>
2022-05-05 21:36:47 -0700

    add login/logout capabilities

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -19,18 +19,14 @@ export async function login({
     where: { username },
   });
   if (!user) return null;
-
   const isCorrectPassword = await bcrypt.compare(
     password,
     user.passwordHash
   );
   if (!isCorrectPassword) return null;
-
   return { id: user.id, username };
 }
 
-// ------------------------------------------------------
-// Session storage
 const sessionSecret = process.env.SESSION_SECRET;
 if (!sessionSecret) {
   throw new Error("SESSION_SECRET must be set");
@@ -50,6 +46,58 @@ const storage = createCookieSessionStorage({
     httpOnly: true,
   },
 });
+
+function getUserSession(request: Request) {
+  return storage.getSession(request.headers.get("Cookie"));
+}
+
+export async function getUserId(request: Request) {
+  const session = await getUserSession(request);
+  const userId = session.get("userId");
+  if (!userId || typeof userId !== "string") return null;
+  return userId;
+}
+
+export async function requireUserId(
+  request: Request,
+  redirectTo: string = new URL(request.url).pathname
+) {
+  const session = await getUserSession(request);
+  const userId = session.get("userId");
+  if (!userId || typeof userId !== "string") {
+    const searchParams = new URLSearchParams([
+      ["redirectTo", redirectTo],
+    ]);
+    throw redirect(`/login?${searchParams}`);
+  }
+  return userId;
+}
+
+export async function getUser(request: Request) {
+  const userId = await getUserId(request);
+  if (typeof userId !== "string") {
+    return null;
+  }
+
+  try {
+    const user = await db.user.findUnique({
+      where: { id: userId },
+      select: { id: true, username: true },
+    });
+    return user;
+  } catch {
+    throw logout(request);
+  }
+}
+
+export async function logout(request: Request) {
+  const session = await getUserSession(request);
+  return redirect("/login", {
+    headers: {
+      "Set-Cookie": await storage.destroySession(session),
+    },
+  });
+}
 
 export async function createUserSession(
   userId: string,
