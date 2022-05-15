39698c86074f564dda93bc1518cc93815831758a
taylor d <taylor.delph@eyecuelab.com>
2022-05-15 14:43:12 -0700

    add optimistic ui for new jokes

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -2,14 +2,16 @@ import type {
   ActionFunction,
   LoaderFunction,
 } from "@remix-run/node";
-import { json, redirect } from "@remix-run/node";
+import { redirect, json } from "@remix-run/node";
 import {
+  Form,
+  Link,
   useActionData,
   useCatch,
-  Link,
-  Form
+  useTransition,
 } from "@remix-run/react";
 
+import { JokeDisplay } from "~/components/joke";
 import { db } from "~/utils/db.server";
 import {
   requireUserId,
@@ -86,6 +88,27 @@ export const action: ActionFunction = async ({
 
 export default function NewJokeRoute() {
   const actionData = useActionData<ActionData>();
+  const transition = useTransition();
+
+  if (transition.submission) {
+    const name = transition.submission.formData.get("name");
+    const content =
+      transition.submission.formData.get("content");
+    if (
+      typeof name === "string" &&
+      typeof content === "string" &&
+      !validateJokeContent(content) &&
+      !validateJokeName(name)
+    ) {
+      return (
+        <JokeDisplay
+          joke={{ name, content }}
+          isOwner={true}
+          canDelete={false}
+        />
+      );
+    }
+  }
 
   return (
     <div>
@@ -171,13 +194,15 @@ export function CatchBoundary() {
     return (
       <div className="error-container">
         <p>You must be logged in to create a joke.</p>
-        <Link prefetch="intent" to="/login">Login</Link>
+        <Link to="/login">Login</Link>
       </div>
     );
   }
 }
 
-export function ErrorBoundary() {
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+
   return (
     <div className="error-container">
       Something unexpected went wrong. Sorry about that.
